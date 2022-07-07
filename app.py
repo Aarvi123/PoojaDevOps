ab236ba3117b5f251db1f85770e64b713839d1ed
SashiDuratech <72461397+SashiDuratech@users.noreply.github.com>
2022-07-07 21:40:58 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,18 @@
+import os
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    return "Welcome to all!"
+
+@app.route('/iiht')
+def hello():
+    return 'Welcome to IIHT'
+    
+@app.route('/duratech')
+def hello():
+    return 'Welcome to Duratech Solutions, Coimbatore'
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
